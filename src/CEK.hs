aac249f6f7cffca6eff6e321625a64ace7642b0b
Bautista Marelli <marellibautista@gmail.com>
2023-10-19 23:22:25 -0300

    feat: implement substN'

--- src/CEK.hs
+++ src/CEK.hs
@@ -74,5 +74,8 @@ evalCEK t = do
     where
         toTerm :: Val -> Term
         toTerm (ValNum n) = Const NoPos (CNat n)
-        toTerm (ValClos (ClosFun p _ f)) = substN (map toTerm (reverse p)) f
-        toTerm (ValClos (ClosFix p _ f)) = substN (map toTerm (reverse p)) f
+        toTerm (ValClos (ClosFun p _ f)) = substN' (map toTerm p) f
+        toTerm (ValClos (ClosFix p _ f)) = substN' (map toTerm p) f
+
+        substN' :: [Term] -> Term -> Term
+        substN' = substN . reverse
