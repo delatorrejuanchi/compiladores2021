fcc588a41224e07609be3220d560c493ed906cba
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2024-02-25 16:11:11 -0300

    feat: fix recursive optimization

--- src/Optimize.hs
+++ src/Optimize.hs
@@ -23,44 +23,50 @@ optimizeDecl :: MonadFD4 m => DeclTerm -> m DeclTerm
 optimizeDecl (Decl p n ty t) = Decl p n ty <$> optimize t
 
 optimize :: MonadFD4 m => Term -> m Term
-optimize t = fst <$> optimize' t
+optimize t = fst <$> optimize' t 0
 
-optimize' :: MonadFD4 m => Term -> m (Term, Bool)
-optimize' t = do
-  (t', changed) <- (constantFolding <||> constantPropagation <||> inlineExpansion <||> recursiveOptimize) t
+optimize' :: MonadFD4 m => Term -> Int -> m (Term, Bool)
+optimize' t d = do
+  (t', changed) <- (constantFolding <||> constantPropagation <||> inlineExpansion <||> recursiveOptimize d) t
   if changed
     then do
-      (t'', _) <- optimize' t'
+      (t'', _) <- optimize' t' d
       return (t'', True)
     else return (t', False)
 
-recursiveOptimize :: MonadFD4 m => Term -> m (Term, Bool)
-recursiveOptimize (Lam p n ty t) = do
-  (t', tChanged) <- optimize' $ open n t
-  return (Lam p n ty (close n t'), tChanged)
-recursiveOptimize (App p u t) = do
-  (u', uChanged) <- optimize' u
-  (t', tChanged) <- optimize' t
+recursiveOptimize :: MonadFD4 m => Int -> Term -> m (Term, Bool)
+recursiveOptimize d (Lam p n ty t) = do
+  let n' = n ++ show d
+  (t', tChanged) <- optimize' (open n' t) (d + 1)
+  return (Lam p n ty (close n' t'), tChanged)
+recursiveOptimize d (App p u t) = do
+  (u', uChanged) <- optimize' u (d + 1)
+  (t', tChanged) <- optimize' t (d + 1)
   return (App p u' t', uChanged || tChanged)
-recursiveOptimize (Print p s t) = (Print p s <.> optimize') t
-recursiveOptimize (Fix p f fty x xty t) = do
-  (t', tChanged) <- optimize' $ openN [x, f] t
-  return (Fix p f fty x xty (closeN [x, f] t'), tChanged)
-recursiveOptimize (IfZ p c t e) = do
-  (c', cChanged) <- optimize' c
-  (t', tChanged) <- optimize' t
-  (e', eChanged) <- optimize' e
+recursiveOptimize d (Print p s t) = do
+  (t', tChanged) <- optimize' t (d + 1)
+  return (Print p s t', tChanged)
+recursiveOptimize d (Fix p f fty x xty t) = do
+  let x' = x ++ show d
+  let f' = f ++ show d
+  (t', tChanged) <- optimize' (openN [x', f'] t) (d + 1)
+  return (Fix p f fty x xty (closeN [x', f'] t'), tChanged)
+recursiveOptimize d (IfZ p c t e) = do
+  (c', cChanged) <- optimize' c (d + 1)
+  (t', tChanged) <- optimize' t (d + 1)
+  (e', eChanged) <- optimize' e (d + 1)
   return (IfZ p c' t' e', cChanged || tChanged || eChanged)
-recursiveOptimize (Let p n ty e t) = do
-  (e', eChanged) <- optimize' e
-  (t', tChanged) <- optimize' $ open n t
-  return (Let p n ty e' (close n t'), eChanged || tChanged)
-recursiveOptimize (BinaryOp p op t u) = do
-  (t', tChanged) <- optimize' t
-  (u', uChanged) <- optimize' u
+recursiveOptimize d (Let p n ty e t) = do
+  let n' = n ++ show d
+  (e', eChanged) <- optimize' e (d + 1)
+  (t', tChanged) <- optimize' (open n' t) (d + 1)
+  return (Let p n ty e' (close n' t'), eChanged || tChanged)
+recursiveOptimize d (BinaryOp p op t u) = do
+  (t', tChanged) <- optimize' t (d + 1)
+  (u', uChanged) <- optimize' u (d + 1)
   return (BinaryOp p op t' u', tChanged || uChanged)
-recursiveOptimize t@(Const _ _) = return (t, False)
-recursiveOptimize t@(V _ _) = return (t, False)
+recursiveOptimize _ t@(Const _ _) = return (t, False)
+recursiveOptimize _ t@(V _ _) = return (t, False)
 
 -- Optimizaciones
 constantFolding :: MonadFD4 m => Term -> m (Term, Bool)
